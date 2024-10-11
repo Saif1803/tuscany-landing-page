0ef7dcba067b2be458fd9ab6d4e90d4a54944940
saif1803 <saif48763@gmail.com>
2024-10-11 22:01:33 +0530

    nav bar scroll effect done

--- script.js
+++ script.js
@@ -13,19 +13,13 @@ window.addEventListener("scroll", () => {
   let scroll = window.scrollY;
   let navbar = document.querySelector(".nav-bar");
   let mobileNav = document.querySelector(".mobile-nav");
-  let links = document.querySelectorAll("a");
+
   if (scroll > 0) {
     navbar.classList.add("scrolled");
     mobileNav.classList.add("scrolled");
-    links.forEach((link) => {
-      link.style.color = "black";
-    });
   } else {
     navbar.classList.remove("scrolled");
     mobileNav.classList.remove("scrolled");
-    links.forEach((link) => {
-      link.style.color = "white";
-    });
   }
 });
 
