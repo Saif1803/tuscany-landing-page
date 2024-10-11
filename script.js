d07a590fc4e7c677efc2f2745a5acf3601b5496a
saif1803 <saif48763@gmail.com>
2024-10-11 20:58:47 +0530

    carousel and company section done

--- script.js
+++ script.js
@@ -8,3 +8,35 @@ hamIcon.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
   hamMenu.classList.toggle("hidden");
 });
+
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+  let navbar = document.querySelector(".nav-bar");
+  let mobileNav = document.querySelector(".mobile-nav");
+  let links = document.querySelectorAll("a");
+  if (scroll > 0) {
+    navbar.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
+    links.forEach((link) => {
+      link.style.color = "black";
+    });
+  } else {
+    navbar.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled");
+    links.forEach((link) => {
+      link.style.color = "white";
+    });
+  }
+});
+
+const carousel = document.querySelector(".carousel");
+const right = document.querySelector(".right");
+const left = document.querySelector(".left");
+
+right.addEventListener("click", () => {
+  carousel.scrollLeft += 200;
+});
+
+left.addEventListener("click", () => {
+  carousel.scrollLeft -= 200;
+});
